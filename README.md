106b259f51a1d88e18bfae677510912994149222
ChloeJo <zhouqiaohui@hotmail.com>
2023-02-01 20:38:37 -0500

    update readme, update todo-app, and the functions are complete

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 - Link: [my Shiny demo](https://chloejoz.shinyapps.io/todo-list-demo/)
 
 # TODO NEXT
-- [ ] Clear the input box after submitting
+- [X] Clear the input box after submitting
 - [X] Add nav bar (update 2023-02-01)
 - [ ] Add more interactive graphics for demonstration
-
+- [X] Add interactive list for checked todo box 
+- [ ] MIGHT move the notice to a dialog box, might not
--- app.R
+++ app.R
@@ -14,11 +14,11 @@ ui <- fluidPage(
                       # Things to do 
                       
                       h3("Daily To Do", id="start", style = "color:darkgery", align = "center"),
-                      div(
-                        class = "form-group mb-3",
-                        textInput("todo", label = "Things I want to do"),
-                        actionButton("todoBnt", class = "btn btn-primary btn-sm", label = "Submit")),
-                      
+                      checkboxGroupInput("checkbox", label = "", choices = c()),
+                      textInput("todo", label = "Things I want to do"),
+                      actionButton("todoBnt", class = "btn btn-primary btn-sm", label = "Submit"),
+                    
+                      br(),
                       br(),
                       
                       # Things Learnt
@@ -29,7 +29,8 @@ ui <- fluidPage(
                     mainPanel(
                       
                       h1(textOutput("timing"), align = "center"),
-                      # h1("What have I done", id = "havedone", align = "center"),
+                      h1("What have I done", id = "havedone", align = "center"),
+                      verbatimTextOutput("value"),
                       h1("What have I learnt", id = "havelearnt", align = "center"),
                       tags$div(
                         tags$ul( id = "list",
@@ -39,16 +40,17 @@ ui <- fluidPage(
                       br(),
                       tags$div(class = "alert alert-dismissible alert-success",
                                br(),
-                               p("Hi, I am building a simple interactive ", span("Todo list", style = "color:darkblue"), "Using Shiny-R.", class = "text-success"),
-                               p("While I do have experience in using React to build web application,"),
-                               p("My experience in Shiny-R is limited,"),
+                               p("Update on 2023-02-01, 8:20pm:", style = "color: black"),
+                               p("Hi, this is the *FINAL* version of a simple interactive ", span("Todo list", style = "color:darkblue"), "Using Shiny-R.", class = "text-success"),
+                               p("While I do have experience in using React to build web application, my experience in Shiny-R is limited."),
                                p("To demonstrate I am a quick learner to new language and tools, and build project based on demands"),
                                p("This small project starts on 2023-01-30, and now you can"),
                                tags$div(
                                  tags$ul(
                                    tags$li("Submit things you want to do to generate a checkbox"),
                                    tags$li("Submit things you have learnt to generate a list"),
-                                   tags$li("Configurating the data that will appear at the title")
+                                   tags$li("Configurating the data that will appear at the title"),
+                                   tags$li("Check the todo list and have the completed task appear on the right")
                                  )
                                ),
                                p("Please still", strong("consider", style = "color:red"),"my application if only applicants who know Shiny are strongly preferred!", class = "text-danger")
@@ -62,22 +64,21 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  choices <- reactiveVal()
+  
   observeEvent(input$todoBnt, {
     
-    print(input$checkbox)
+    if(nchar(input$todo)>0){
     
-    name <- paste0("checkbox",gsub(" ","",input$todo))
-
-    if( nchar(input$todo) >0 ){insertUI(
-      selector = "#start",
-      where = "afterEnd",
-      ui = checkboxInput(name,input$todo)
-    )}
-  })
+    selected <- choices()[choices() %in% input$checkbox]
+    choices(c(choices(), input$todo))
+    updateCheckboxGroupInput(inputId = "checkbox", choices = choices(), selected = selected)
+    updateTextInput(inputId = "todo", value = "")}
+    
+      })
 
     
   observeEvent(input$learntBnt, {
-    print("triggered")
     
     if( nchar(input$learnt) > 0){
       
@@ -86,14 +87,18 @@ server <- function(input, output) {
         where = "beforeEnd",
         ui = tags$li(input$learnt)
       )
+      
+      updateTextInput(inputId = "learnt", value = "")
     }
   })
 
   output$timing <- renderText({ 
     paste0("Today is ", as.character(input$date))
   })
-  
-  output$value <- renderText({ input$checkbox })
+
+  output$value <- renderText({ 
+    paste("> ",input$checkbox, collapse = "\n")
+  })
   
 }
 
